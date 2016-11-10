fa164a1bd317364821b1aa060c51d8bf202b7909
Carlos Orellana <carlos.cw2106@gmail.com>
2016-11-10 11:06:03 -0600

    setup rspec

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@ Rails.application.routes.draw do
   # Api definition
   namespace :api, defaults: { format: :json },
                               constraints: { subdomain: 'api' }, path: '/'  do
-    #
+    scope module: :v1 do
+      #
+    end
   end
 end
